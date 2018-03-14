91579aa211237c9b729cd762f0e9465a7fec7735
Vera Butler <verabutlerdesign@gmail.com>
2018-03-13 20:55:31 -0400

    added reducer folders/files

--- client/src/components/cardTW.js
+++ client/src/components/cardTW.js
@@ -1,19 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Time from '../containers/time';
 import Weather from '../containers/weather';
 import { TW } from '../styles/--weather';
 
-class CardTW extends React.Component{
+ 
+class CardTW extends Component{
     render() {
         return (
-                <TW>
-                    <Time />
-                    <Weather />
-                </TW>
-           
+            <TW>
+                <Time />
+                <Weather />
+            </TW>
         );
     }
-
 }
 
 export default CardTW;
