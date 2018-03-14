cfa9aabbdb5ddebad5fdd7c307ece10bcdf7646a
LucasTaboada <ltlucastaboada@gmail.com>
2018-03-13 20:59:12 -0400

    testing

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import Time from './containers/time';
-import Weather from './containers/weather';
+import Time from './Containers/time';
+import Weather from './Containers/weather';
 
 class App extends Component {
   render() {
