dde8554e7ea116a2a0fe53d98048089cd3a798e7
LucasTaboada <ltlucastaboada@gmail.com>
2018-04-05 19:52:56 -0400

    add signup and signin

--- client/src/container/backgroundImage.js
+++ client/src/container/backgroundImage.js
@@ -8,7 +8,7 @@ componentDidMount() {
     backgroundStyle.backgroundImage = `url("../styles/default-image/default-image.jpeg")`;
     backgroundStyle.backgroundImage = `url("https://source.unsplash.com/collection/1851577/")`;
     backgroundStyle.backgroundRepeat = "no-repeat";
-    // backgroundStyle.backgroundSize = "cover";
+    backgroundStyle.backgroundSize = "cover";
     backgroundStyle.position = "absolute";
   }
 
