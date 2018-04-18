3dabe77f87815e4668118d73d637510d9368d2dd
Vera Butler <verabutlerdesign@gmail.com>
2018-04-18 17:18:07 -0400

    update

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 // makae sure user is logged in = Authentication
 exports.loginRequired = function(req,res,next){
     try{
-        const token = req.headers.authorization.split(' ')[1];
+        const token = headers.authorization.split(' ')[1];
         jwt.verify(token,process.env.SECRET_KEY, function(err,decoded){
             if(decoded){
                 return next();
