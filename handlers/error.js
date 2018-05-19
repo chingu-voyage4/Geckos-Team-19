06de480b2e7c6fe2fce4e6b3db0057ad3203b6eb
VeraButler <verabutlerdesign@gmail.com>
2018-05-19 13:40:05 -0400

    minor clean up front end

--- handlers/error.js
+++
@@ -1,8 +0,0 @@
-function errorHander(error, request, response, next){
-    return response.status(error.status|| 500).json({
-        error:{
-            message: error.message || 'Oops! Something went wrong.'
-        }
-    });
-}
-module.exports = errorHander;
--- routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router()
-const { signup, signin } = require('../handlers/auth');
-
-router.post('/signup', signup);
-router.post('/signin', signin);
-
-module.exports = router;
